364f1ae5e2adcd50f99137ad16c2b0e9554f08ef
Anne Chen <annechen112995@gmail.com>
2023-03-01 17:27:40 -0800

    Update cors origins

--- src/cors.ts
+++ src/cors.ts
@@ -2,14 +2,18 @@ let origin: string[] = [];
 
 // Loosen CORS for development and staging
 if (process.env.NODE_ENV === 'production') {
-  origin = [ 'https://cro-matic.com' ];
+  origin = [ 'https://platform.cro-matic.com' ];
+} else if (process.env.NODE_ENV === 'production') {
+  origin = [
+    'https://caesar-staging.herokuapp.com',
+    'https://octavian-staging.herokuapp.com',
+  ];
 } else {
   origin = [
     'http://localhost:3000',
     'https://localhost:3000',
     'http://localhost:9000',
     'https://localhost:9000',
-    'https://cromatic-staging.herokuapp.com',
     'https://studio.apollographql.com',
   ];
 }
