280b811d583ce19a1f204fe12f7045c6759b39ef
Katherine <lixi900620@gmail.com>
2023-04-07 23:12:32 -0500

    feat: add AdminInviteNotification function

---
+++ src/notification/adminInviteNotification.ts
@@ -0,0 +1,39 @@
+import { InternalError } from '../graphql/errors/InternalError';
+import { prisma } from '../connectDB';
+
+const createAdminInviteNotification = async (sender_id: string, recipient_id: string, project_connection_id: string) => {
+  const recipient = await prisma.user.findFirst({
+    where: {
+      id: recipient_id,
+    },
+  });
+  if (!recipient) {
+    throw new InternalError('Recipient not found');
+  }
+
+  const project_connection = await prisma.projectConnection.findFirst({
+    where: {
+      id: project_connection_id,
+    },
+    include: {
+      project_request: true,
+    },
+  });
+
+  const notification = await prisma.notification.create({
+    data: {
+      notification_type: 'AdminInviteNotification',
+      message: `**Cromatic Admin** invited you to review a new request has been submitted by a Cromatic client, the project name is **${project_connection?.project_request.title}**`,
+      params: {
+        project_connection_id: project_connection_id,
+      },
+      recipient_id: recipient_id,
+    },
+  });
+
+  if (!notification) {
+    throw new InternalError('Notification not created');
+  }
+};
+
+export default createAdminInviteNotification;
