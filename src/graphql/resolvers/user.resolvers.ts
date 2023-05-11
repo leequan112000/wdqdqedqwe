66de62d7c29f0861063dcab673e37b85c40e076d
Katherine <lixi900620@gmail.com>
2023-05-11 14:31:39 -0500

    feat: remove unused comment

--- src/graphql/resolvers/user.resolvers.ts
+++ src/graphql/resolvers/user.resolvers.ts
@@ -508,7 +508,6 @@ const resolvers: Resolvers<Context> = {
         throw new PublicError('User not found.');
       }
 
-      // extend token expiration to 3 days
       const resetTokenExpiration = new Date().getTime() + 3 * 24 * 60 * 60 * 1000;
       const updatedUser = await context.prisma.user.update({
         where: {
