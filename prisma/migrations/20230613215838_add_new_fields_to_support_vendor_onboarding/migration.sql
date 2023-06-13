9c3554e3d990c32782e81a7e2177022a1a40e97e
Katherine <lixi900620@gmail.com>
2023-06-13 17:00:35 -0500

    feat: update many to many table name

--- prisma/migrations/20230613215838_add_new_fields_to_support_vendor_onboarding/migration.sql
+++ prisma/migrations/20230613215838_add_new_fields_to_support_vendor_onboarding/migration.sql
@@ -15,14 +15,14 @@ CREATE TABLE "certification_tags" (
 );
 
 -- CreateTable
-CREATE TABLE "certification_tags_on_vendor_companies" (
+CREATE TABLE "certification_tag_connections" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "vendor_company_id" UUID NOT NULL,
     "certification_tag_id" UUID NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "certification_tags_on_vendor_companies_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "certification_tag_connections_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -38,36 +38,36 @@ CREATE TABLE "lab_specializations" (
 );
 
 -- CreateTable
-CREATE TABLE "lab_specializations_on_vendor_companies" (
+CREATE TABLE "lab_specialization_connections" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "vendor_company_id" UUID NOT NULL,
     "lab_specialization_id" UUID NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "lab_specializations_on_vendor_companies_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "lab_specialization_connections_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "certification_tags_full_name_key" ON "certification_tags"("full_name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "certification_tags_on_vendor_companies_vendor_company_id_ce_key" ON "certification_tags_on_vendor_companies"("vendor_company_id", "certification_tag_id");
+CREATE UNIQUE INDEX "certification_tag_connections_vendor_company_id_certificati_key" ON "certification_tag_connections"("vendor_company_id", "certification_tag_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "lab_specializations_full_name_key" ON "lab_specializations"("full_name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "lab_specializations_on_vendor_companies_vendor_company_id_l_key" ON "lab_specializations_on_vendor_companies"("vendor_company_id", "lab_specialization_id");
+CREATE UNIQUE INDEX "lab_specialization_connections_vendor_company_id_lab_specia_key" ON "lab_specialization_connections"("vendor_company_id", "lab_specialization_id");
 
 -- AddForeignKey
-ALTER TABLE "certification_tags_on_vendor_companies" ADD CONSTRAINT "certification_tags_on_vendor_companies_vendor_company_id_fkey" FOREIGN KEY ("vendor_company_id") REFERENCES "vendor_companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "certification_tag_connections" ADD CONSTRAINT "certification_tag_connections_vendor_company_id_fkey" FOREIGN KEY ("vendor_company_id") REFERENCES "vendor_companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "certification_tags_on_vendor_companies" ADD CONSTRAINT "certification_tags_on_vendor_companies_certification_tag_i_fkey" FOREIGN KEY ("certification_tag_id") REFERENCES "certification_tags"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "certification_tag_connections" ADD CONSTRAINT "certification_tag_connections_certification_tag_id_fkey" FOREIGN KEY ("certification_tag_id") REFERENCES "certification_tags"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "lab_specializations_on_vendor_companies" ADD CONSTRAINT "lab_specializations_on_vendor_companies_vendor_company_id_fkey" FOREIGN KEY ("vendor_company_id") REFERENCES "vendor_companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "lab_specialization_connections" ADD CONSTRAINT "lab_specialization_connections_vendor_company_id_fkey" FOREIGN KEY ("vendor_company_id") REFERENCES "vendor_companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "lab_specializations_on_vendor_companies" ADD CONSTRAINT "lab_specializations_on_vendor_companies_lab_specialization_fkey" FOREIGN KEY ("lab_specialization_id") REFERENCES "lab_specializations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "lab_specialization_connections" ADD CONSTRAINT "lab_specialization_connections_lab_specialization_id_fkey" FOREIGN KEY ("lab_specialization_id") REFERENCES "lab_specializations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
