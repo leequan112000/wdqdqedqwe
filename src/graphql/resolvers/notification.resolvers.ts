2e90c3357dd4604f2c49254366c5934abb1143bf
Shi Jin Ting <shijinting96@gmail.com>
2023-07-03 10:47:09 +0800

    Fixed notification to sorted by created_at desc

--- src/graphql/resolvers/notification.resolvers.ts
+++ src/graphql/resolvers/notification.resolvers.ts
@@ -65,7 +65,7 @@ const resolvers: Resolvers<Context> = {
           ...(!!args.unread_only ? { read_at: null } : {}),
         },
         orderBy: {
-          updated_at: 'desc'
+          created_at: 'desc'
         }
       });
     },
