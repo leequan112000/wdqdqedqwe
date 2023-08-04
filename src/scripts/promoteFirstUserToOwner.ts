d1fe09a7898a093c5769cc3c4f996e1ca98dc0c5
Haosiong Ng <hao@cro-matic.com>
2023-08-04 19:51:49 +0800

    chore: create customer and vendor member connection if havent

--- src/scripts/promoteFirstUserToOwner.ts
+++ src/scripts/promoteFirstUserToOwner.ts
@@ -54,7 +54,8 @@ const promoteFirstUserToOwner = async () => {
       }, []);
 
       const updateBiotechUserTasks = biotechUsers.map(async (u) => {
-        await trx.customer.update({
+        // Promote to owner
+        const customer = await trx.customer.update({
           where: {
             user_id: u.id,
           },
@@ -62,12 +63,46 @@ const promoteFirstUserToOwner = async () => {
             role: CompanyCollaboratorRoleType.OWNER,
           },
         });
+
+        // Build customer connection if haven't
+        const projectConnections = await trx.projectConnection.findMany({
+          where: {
+            project_request: {
+              biotech_id: customer.biotech_id,
+            },
+            customer_connections: {
+              none: {
+                customer_id: customer.id,
+              }
+            }
+          },
+        });
+        const upsertTasks = projectConnections.map(async (pc) => {
+          return await trx.customerConnection.upsert({
+            create: {
+              customer_id: customer.id,
+              project_connection_id: pc.id,
+            },
+            update: {
+              customer_id: customer.id,
+              project_connection_id: pc.id,
+            },
+            where: {
+              project_connection_id_customer_id: {
+                customer_id: customer.id,
+                project_connection_id: pc.id,
+              }
+            }
+          })
+        });
+        await Promise.all(upsertTasks);
 
         await addRoleForUser(u.id, CasbinRole.OWNER);
       });
 
       const updateVendorUserTasks = vendorUsers.map(async (u) => {
-        await trx.vendorMember.update({
+        // Promote to owner
+        const vendorMember = await trx.vendorMember.update({
           where: {
             user_id: u.id,
           },
@@ -75,6 +110,37 @@ const promoteFirstUserToOwner = async () => {
             role: CompanyCollaboratorRoleType.OWNER,
           },
         });
+
+        // Build vendor member connection if haven't
+        const projectConnections = await trx.projectConnection.findMany({
+          where: {
+            vendor_company_id: vendorMember.vendor_company_id,
+            vendor_member_connections: {
+              none: {
+                vendor_member_id: vendorMember.id,
+              },
+            },
+          },
+        });
+        const upsertTasks = projectConnections.map(async (pc) => {
+          await trx.vendorMemberConnection.upsert({
+            create: {
+              vendor_member_id: vendorMember.id,
+              project_connection_id: pc.id,
+            },
+            update: {
+              vendor_member_id: vendorMember.id,
+              project_connection_id: pc.id,
+            },
+            where: {
+              project_connection_id_vendor_member_id: {
+                vendor_member_id: vendorMember.id,
+                project_connection_id: pc.id,
+              },
+            },
+          });
+        });
+        await Promise.all(upsertTasks);
 
         await addRoleForUser(u.id, CasbinRole.OWNER);
       });
