e5a7e8d9605942ec101d0b2fa2787375876be538
Katherine <lixi900620@gmail.com>
2023-08-07 19:38:44 -0500

    feat: add feature flag to updateBiotechInviteVendor mutation

--- src/graphql-admin/resolvers/biotechInviteVendor.resolvers.ts
+++ src/graphql-admin/resolvers/biotechInviteVendor.resolvers.ts
@@ -6,27 +6,30 @@ import invariant from "../../helper/invariant";
 const resolvers: Resolvers<Context> = {
   Mutation: {
     updateBiotechInviteVendor: async (_, args, context) => {
-      const biotechInviteVendor = await context.prisma.biotechInviteVendor.findFirst({
-        where: {
-          id: args.id,
-        },
-      });
-      invariant(biotechInviteVendor, new PublicError('Biotech invite vendor not found.'));
+      if (process.env.BIOTECH_INVITE_CRO) {
+        const biotechInviteVendor = await context.prisma.biotechInviteVendor.findFirst({
+          where: {
+            id: args.id,
+          },
+        });
+        invariant(biotechInviteVendor, new PublicError('Biotech invite vendor not found.'));
 
-      const updatedBiotechInviteVendor = await context.prisma.biotechInviteVendor.update({
-        where: {
-          id: biotechInviteVendor.id,
-        },
-        data: {
-          company_name: args.company_name,
-          website: args.website,
-          first_name: args.first_name,
-          last_name: args.last_name,
-          email: args.email,
-        },
-      });
+        const updatedBiotechInviteVendor = await context.prisma.biotechInviteVendor.update({
+          where: {
+            id: biotechInviteVendor.id,
+          },
+          data: {
+            company_name: args.company_name,
+            website: args.website,
+            first_name: args.first_name,
+            last_name: args.last_name,
+            email: args.email,
+          },
+        });
 
-      return updatedBiotechInviteVendor;
+        return updatedBiotechInviteVendor;
+      }
+      return null;
     },
   }
 };
