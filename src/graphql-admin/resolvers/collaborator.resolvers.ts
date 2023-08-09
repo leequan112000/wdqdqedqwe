57f3de30af8a153bcced3b58c9185a7b698843e4
Haosiong Ng <hao@cro-matic.com>
2023-08-09 15:25:33 +0800

    chore: support retool update vendor member

--- src/graphql-admin/resolvers/collaborator.resolvers.ts
+++ src/graphql-admin/resolvers/collaborator.resolvers.ts
@@ -4,6 +4,8 @@ import { Context } from "../../types/context";
 import { sendVendorMemberInvitationByAdminEmail } from "../../mailer/vendorMember";
 import invariant from "../../helper/invariant";
 import { PublicError } from "../../graphql/errors/PublicError";
+import collaboratorService from '../../services/collaborator/collaborator.service';
+import { CompanyCollaboratorRoleType } from "../../helper/constant";
 
 const resolvers: Resolvers<Context> = {
   Mutation: {
@@ -39,6 +41,59 @@ const resolvers: Resolvers<Context> = {
 
       sendVendorMemberInvitationByAdminEmail(updatedNewUser);
 
+      return true;
+    },
+    updateVendorMemberByAdmin: async (_, args, context) => {
+      const { user_id, department, first_name, last_name, role, title } = args;
+      await context.prisma.$transaction(async (trx) => {
+        await trx.user.update({
+          where: {
+            id: user_id,
+          },
+          data: {
+            first_name: first_name ?? undefined,
+            last_name: last_name ?? undefined,
+          },
+        });
+        const updatedVendorMember = await trx.vendorMember.update({
+          where: {
+            user_id,
+          },
+          data: {
+            title: title ?? undefined,
+            department: department ?? undefined,
+          },
+        });
+
+        switch (role) {
+          case CompanyCollaboratorRoleType.ADMIN: {
+            await collaboratorService.setVendorMemberAsAdmin(
+              {
+                user_id,
+                vendor_company_id: updatedVendorMember.vendor_company_id,
+                vendor_member_id: updatedVendorMember.id,
+              },
+              { prisma: trx },
+            );
+            break;
+          }
+          case CompanyCollaboratorRoleType.USER: {
+            await collaboratorService.setVendorMemberAsUser(
+              {
+                vendor_member_id: updatedVendorMember.id,
+              },
+              { prisma: trx },
+            );
+            break;
+          }
+          case CompanyCollaboratorRoleType.OWNER: {
+            // ignore
+            break;
+          }
+          default:
+            throw new PublicError('Invalid role');
+        }
+      });
       return true;
     },
   }
