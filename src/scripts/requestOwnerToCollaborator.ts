347d456d09c73da989ddb0d047a6426c76506eca
Shi Jin Ting <shijinting96@gmail.com>
2023-08-11 08:54:21 +0800

    Added script to add existing project request owner to request collaborator

---
+++ src/scripts/requestOwnerToCollaborator.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  try {
+    await prisma.$transaction(async (trx) => {
+      // Find project request owner
+      const allProjectRequests = await trx.projectRequest.findMany();
+
+      const upsertProjectRequestCollaborators = allProjectRequests.map(async (request) => {
+        return await trx.projectRequestCollaborator.upsert({
+          create: {
+            customer_id: request.customer_id,
+            project_request_id: request.id,
+          },
+          update: {
+            customer_id: request.customer_id,
+            project_request_id: request.id,
+          },
+          where: {
+            project_request_id_customer_id: {
+              customer_id: request.customer_id,
+              project_request_id: request.id,
+            }
+          }
+        })
+      });
+
+      await Promise.all(upsertProjectRequestCollaborators);
+      console.log('Operation succeeded.')
+    });
+  } catch (error) {
+    console.log('Operation failed.')
+    console.log(error)
+  } finally {
+    await prisma.$disconnect();
+  }
+  process.exit(0);
+}
+
+main();
