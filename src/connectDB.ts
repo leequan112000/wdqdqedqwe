24a66f854f2e5542d12f01057c6d649075d6e753
Haosiong Ng <hao@cro-matic.com>
2023-08-18 12:21:30 +0800

    chore: remove explicit connection & disconnect

--- src/connectDB.ts
+++ src/connectDB.ts
@@ -9,18 +9,3 @@ export const prisma = global.prisma || new PrismaClient({ log: ['info'] });
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
 }
-
-async function connectDB() {
-  try {
-    await prisma.$connect();
-    console.log('Database connected successfully');
-  } catch (error) {
-    console.log(error);
-    await prisma.$disconnect();
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-
-export default connectDB;
