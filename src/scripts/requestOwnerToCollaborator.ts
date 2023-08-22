f61850ed5a9043b23e6a7040ed0114edd2a0f5c2
Haosiong Ng <hao@cro-matic.com>
2023-08-22 17:31:18 +0800

    chore: optimize setup role scripts with longer timeout and batched processing

--- src/scripts/requestOwnerToCollaborator.ts
+++ src/scripts/requestOwnerToCollaborator.ts
@@ -1,33 +1,38 @@
 import { PrismaClient } from "@prisma/client";
+import { PromisePool } from '@supercharge/promise-pool';
 
 const prisma = new PrismaClient();
 
 const main = async () => {
   try {
+    const allProjectRequests = await prisma.projectRequest.findMany();
     await prisma.$transaction(async (trx) => {
-      const allProjectRequests = await trx.projectRequest.findMany();
-      const upsertProjectRequestCollaborators = allProjectRequests.map(async (request) => {
-        return await trx.projectRequestCollaborator.upsert({
-          create: {
-            customer_id: request.customer_id,
-            project_request_id: request.id,
-          },
-          update: {
-            customer_id: request.customer_id,
-            project_request_id: request.id,
-          },
-          where: {
-            project_request_id_customer_id: {
+      await PromisePool
+        .withConcurrency(10)
+        .for(allProjectRequests)
+        .process(async (request) => {
+          console.log('Processing:', request.title);
+
+          await trx.projectRequestCollaborator.upsert({
+            create: {
               customer_id: request.customer_id,
               project_request_id: request.id,
+            },
+            update: {
+              customer_id: request.customer_id,
+              project_request_id: request.id,
+            },
+            where: {
+              project_request_id_customer_id: {
+                customer_id: request.customer_id,
+                project_request_id: request.id,
+              }
             }
-          }
-        })
-      });
+          })
+        });
 
-      await Promise.all(upsertProjectRequestCollaborators);
       console.log('Operation succeeded.')
-    });
+    }, { timeout: 90000 });
   } catch (error) {
     console.log('Operation failed.')
     console.log(error)
---
+++ src/scripts/setupVendorRegularUserRole.ts
@@ -0,0 +1,71 @@
+import { User } from '@prisma/client';
+import { PromisePool } from '@supercharge/promise-pool';
+import prisma from '../prisma';
+import { addRoleForUser } from '../helper/casbin';
+import { CasbinRole } from '../helper/constant';
+
+const main = async () => {
+  try {
+    const vendorCompaniesWithFirstUser = await prisma.vendorCompany.findMany({
+      select: {
+        vendor_members: {
+          select: {
+            user: true,
+          },
+          orderBy: {
+            user: {
+              created_at: 'asc',
+            },
+          },
+          take: 1,
+        },
+      },
+    });
+    const vendorFirstUsers = vendorCompaniesWithFirstUser.reduce<User[]>((acc, b) => {
+      if (b.vendor_members.length > 0) {
+        acc.unshift(b.vendor_members[0].user);
+      }
+      return acc;
+    }, []);
+    const vendorCompanyFirstUserIDs = vendorFirstUsers.map((u) => u.id);
+    const vendorCompanyWithRegularUsers = await prisma.vendorCompany.findMany({
+      select: {
+        vendor_members: {
+          select: {
+            user: true,
+          },
+          where: {
+            user_id: {
+              notIn: vendorCompanyFirstUserIDs,
+            },
+          },
+        },
+      },
+    });
+    const vendorCompanyRegularUsers = vendorCompanyWithRegularUsers.reduce<User[]>((acc, v) => {
+      if (v.vendor_members.length > 0) {
+        const users = v.vendor_members.map((vm) => vm.user);
+        acc.unshift(...users);
+      }
+      return acc;
+    }, []);
+
+    await PromisePool
+      .withConcurrency(10)
+      .for(vendorCompanyRegularUsers)
+      .process(async (u) => {
+        console.log('Processing:', u.first_name, u.last_name);
+        await addRoleForUser(u.id, CasbinRole.USER);
+      });
+
+    console.log('Operation done.')
+  } catch (error) {
+    console.log('Operation failed.')
+    console.log(error)
+  } finally {
+    await prisma.$disconnect();
+  }
+  process.exit(0);
+}
+
+main();
