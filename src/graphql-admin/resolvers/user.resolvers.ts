3177475c7e8496565a4088760ac7839dd349758a
Shi Jin Ting <shijinting96@gmail.com>
2023-08-23 17:31:35 +0800

    Added purgeTestDataByUser & unregisterBiotechAccount mutations

---
+++ src/graphql-admin/resolvers/user.resolvers.ts
@@ -0,0 +1,32 @@
+import userService from '../../services/user/user.service';
+import { Resolvers } from '../../generated';
+import { Context } from '../../types/context';
+
+const resolver: Resolvers<Context> = {
+  Mutation: {
+    purgeTestDataByUser: async (_, args, context) => {
+      const { user_id } = args;
+    
+      return await context.prisma.$transaction(async (trx) => {
+        return userService.purgeTestDataByUser({
+          user_id
+        }, {
+          prisma: trx
+        });
+      });
+    },
+    unregisterBiotechAccount: async (_, args, context) => {
+      const { biotech_id } = args;
+    
+      return await context.prisma.$transaction(async (trx) => {
+        return userService.unregisterBiotechAccount({
+          biotech_id
+        }, {
+          prisma: trx
+        });
+      });
+    },
+  }
+}
+
+export default resolver;
