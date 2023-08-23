50be32bb72d996441d0a59776345dd6a61c7b135
Shi Jin Ting <shijinting96@gmail.com>
2023-08-23 23:47:07 +0800

    Increased purge data prisma txn timeout

--- src/graphql-admin/resolvers/user.resolvers.ts
+++ src/graphql-admin/resolvers/user.resolvers.ts
@@ -6,18 +6,21 @@ const resolver: Resolvers<Context> = {
   Mutation: {
     purgeTestDataByUser: async (_, args, context) => {
       const { user_id } = args;
-    
+
       return await context.prisma.$transaction(async (trx) => {
         return userService.purgeTestDataByUser({
           user_id
         }, {
           prisma: trx
         });
+      }, {
+        maxWait: 10000,
+        timeout: 40000,
       });
     },
     unregisterBiotechAccount: async (_, args, context) => {
       const { biotech_id } = args;
-    
+
       return await context.prisma.$transaction(async (trx) => {
         return userService.unregisterBiotechAccount({
           biotech_id
