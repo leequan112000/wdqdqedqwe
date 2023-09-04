66822549da2707f962ac53be239ecac7d62394db
Shi Jin Ting <shijinting96@gmail.com>
2023-09-04 15:16:55 +0800

    Added getFileExtFromBuffer function & updated storeUpload function

--- src/helper/storeUpload.ts
+++ src/helper/storeUpload.ts
@@ -1,6 +1,7 @@
 import { HeadObjectCommand } from "@aws-sdk/client-s3";
 import { Upload } from '@aws-sdk/lib-storage'
 import { randomUUID } from "crypto";
+import { fromBuffer } from "detect-file-type";
 import { extname } from 'path';
 import mime from 'mime-types';
 import { s3Client } from "./awsS3";
@@ -15,7 +16,7 @@ export default async function storeUpload(upload: UploadParamType, path: string 
   try {
     const { createReadStream, filename } = upload;
     const mimeType = mime.lookup(filename);
-    const contextType = typeof mimeType === 'string' ? mimeType : undefined;
+    const contentType = typeof mimeType === 'string' ? mimeType : undefined;
 
     const key = `${process.env.NODE_ENV}/${path ? `${path}/` : ''}${randomUUID()}${extname(filename)}`;
     const s3Upload = new Upload({
@@ -24,7 +25,7 @@ export default async function storeUpload(upload: UploadParamType, path: string 
         Body: createReadStream(),
         Bucket: isPublic ? config.s3.publicBucket : config.s3.bucket,
         Key: key,
-        ContentType: contextType,
+        ContentType: contentType,
         ...(isPublic? { ACL: "public-read" } : {})
       },
     });
@@ -32,7 +33,7 @@ export default async function storeUpload(upload: UploadParamType, path: string 
     await s3Upload.done();
 
     const headObjectCommand = new HeadObjectCommand({
-      Bucket: config.s3.bucket,
+      Bucket: isPublic ? config.s3.publicBucket : config.s3.bucket,
       Key: key,
     });
     const headObjectResp = await s3Client.send(headObjectCommand)
@@ -42,10 +43,22 @@ export default async function storeUpload(upload: UploadParamType, path: string 
       filename,
       bucket: isPublic ? config.s3.publicBucket : config.s3.bucket,
       filesize: headObjectResp.ContentLength || 0,
-      contextType,
+      contentType,
     }
   } catch (error) {
     console.log(error);
     throw error;
   }
+}
+
+export const getFileExtFromBuffer = (buffer: Buffer) => {
+  return new Promise((resolve, reject) => {
+    fromBuffer(buffer, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+   
+      resolve(result.ext)
+    });
+  })
 }
