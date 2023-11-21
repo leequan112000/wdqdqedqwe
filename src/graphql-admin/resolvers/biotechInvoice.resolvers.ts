05220adb41f3b4591cf63a7b8c54387163970ffb
Shi Jin Ting <shijinting96@gmail.com>
2023-11-21 12:14:32 +0800

    Implemented biotech invoice payment verified email & notification jobs

--- src/graphql-admin/resolvers/biotechInvoice.resolvers.ts
+++ src/graphql-admin/resolvers/biotechInvoice.resolvers.ts
@@ -1,9 +1,15 @@
+import moment from "moment";
+import { app_env } from "../../environment";
 import { Resolvers } from "../generated";
 import { Context } from "../../types/context";
-import { InvoicePaymentDisplayStatus, InvoicePaymentStatus } from "../../helper/constant";
+import { CompanyCollaboratorRoleType, InvoicePaymentDisplayStatus, InvoicePaymentStatus } from "../../helper/constant";
 import invariant from "../../helper/invariant";
 import { toDollar } from "../../helper/money";
 import { PublicError } from "../../graphql/errors/PublicError";
+
+import { bulkBiotechInvoicePaymentVerifiedByCromaticAdminEmail } from "../../mailer/biotechInvoice";
+import { createNotificationQueueJob } from "../../queues/notification.queues";
+import { createBiotechInvoicePaymentVerifiedNotificationJob } from "../../notification/biotechInvoiceNotification";
 
 const resolvers: Resolvers<Context> = {
   BiotechInvoice: {
@@ -73,6 +79,8 @@ const resolvers: Resolvers<Context> = {
         include: {
           purchase_order: true,
           blanket_purchase_order_transaction: true,
+          biotech_invoice_items: true,
+          biotech: true,
         }
       });
 
@@ -80,6 +88,41 @@ const resolvers: Resolvers<Context> = {
       invariant(biotechInvoice.purchase_order || biotechInvoice.blanket_purchase_order_transaction, new PublicError('This invoice is paid via ACH'));
       invariant(biotechInvoice.payment_status !== InvoicePaymentStatus.PAID, new PublicError('Invoice has already paid'));
       invariant(biotechInvoice.payment_status === InvoicePaymentStatus.PROCESSING, new PublicError('Invoice payment status invalid to perform this action'));
+
+      const receivers = await context.prisma.customer.findMany({
+        where: {
+          biotech_id: biotechInvoice.biotech_id,
+          role: CompanyCollaboratorRoleType.OWNER,
+          user: {
+            is_active: true,
+          },
+        },
+        include: {
+          user: true,
+        }
+      });
+
+      const totalAmount = biotechInvoice.biotech_invoice_items.reduce((acc, item) => acc + item.amount.toNumber(), 0);
+      const emailData = receivers.map((r) => ({
+        emailData: {
+          invoice_date: moment(biotechInvoice.created_at).format('ll'),
+          invoice_number: biotechInvoice.invoice_number,
+          invoice_total_amount: toDollar(totalAmount).toString(),
+          biotech_company_name: biotechInvoice.biotech.name,
+          button_url: `${app_env.APP_URL}/app/invoices/${biotechInvoice.id}`
+        },
+        receiverEmail: r.user.email,
+      }));
+      const notificationData = receivers.map((r) => {
+        return createBiotechInvoicePaymentVerifiedNotificationJob({
+          recipient_id: r.id,
+          invoice_id: biotechInvoice.id,
+          invoice_number: biotechInvoice.invoice_number,
+          invoice_total_amount: toDollar(totalAmount).toString(),
+        })
+      })
+      bulkBiotechInvoicePaymentVerifiedByCromaticAdminEmail(emailData);
+      createNotificationQueueJob({ data: notificationData });
 
       return await context.prisma.biotechInvoice.update({
         where: {
