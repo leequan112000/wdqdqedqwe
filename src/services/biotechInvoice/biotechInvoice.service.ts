b71e1e22c18ccafd4837c4fd98bf0fb3288df8b8
Shi Jin Ting <shijinting96@gmail.com>
2023-11-21 12:14:07 +0800

    Implemented new biotech invoice email & notification jobs

--- src/services/biotechInvoice/biotechInvoice.service.ts
+++ src/services/biotechInvoice/biotechInvoice.service.ts
@@ -1,9 +1,16 @@
 import { Milestone, Prisma } from '@prisma/client';
 import moment from 'moment';
 import currency from 'currency.js';
+
+import { app_env } from '../../environment';
+import { bulkNewBiotechInvoiceNoticeEmail } from '../../mailer/biotechInvoice';
+import { createNewBiotechInvoiceNotificationJob } from '../../notification/biotechInvoiceNotification';
+import { createNotificationQueueJob } from '../../queues/notification.queues';
 import { ServiceContext } from '../../types/context';
-import { InvoicePaymentStatus } from '../../helper/constant';
+
+import { CompanyCollaboratorRoleType, InvoicePaymentStatus } from '../../helper/constant';
 import { generateInvoiceNumber, generateInvoiceReferenceId } from '../../helper/invoice';
+import { toDollar } from '../../helper/money';
 
 export type CreateBiotechInvoiceArgs = {
   milestone: Milestone
@@ -22,7 +29,7 @@ export const createBiotechInvoice = async (args: CreateBiotechInvoiceArgs, conte
     milestone_id: milestone.id,
   };
 
-  const invoice = await context.prisma.biotechInvoice.create({
+  const biotechInvoice = await context.prisma.biotechInvoice.create({
     data: {
       invoice_number: generateInvoiceNumber(true),
       payment_status: payViaStripe ? InvoicePaymentStatus.PROCESSING : InvoicePaymentStatus.UNPAID,
@@ -33,9 +40,66 @@ export const createBiotechInvoice = async (args: CreateBiotechInvoiceArgs, conte
       biotech_id,
       reference_id: generateInvoiceReferenceId(),
     },
+    include: {
+      biotech_invoice_items: {
+        include: {
+          milestone: {
+            include: {
+              quote: {
+                include: {
+                  project_connection: {
+                    include: {
+                      vendor_company: true,
+                      project_request: true,
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      biotech: true,
+    }
   });
-  
-  return invoice;
+
+  const receivers = await context.prisma.customer.findMany({
+    where: {
+      biotech_id: biotechInvoice.biotech_id,
+      role: CompanyCollaboratorRoleType.OWNER,
+      user: {
+        is_active: true,
+      },
+    },
+    include: {
+      user: true,
+    }
+  });
+
+  const totalAmount = biotechInvoice.biotech_invoice_items.reduce((acc, item) => acc + item.amount.toNumber(), 0);
+  const emailData = receivers.map((r) => ({
+    emailData: {
+      project_title: biotechInvoice.biotech_invoice_items[0].milestone?.quote.project_connection.project_request.title as string,
+      invoice_number: biotechInvoice.invoice_number,
+      invoice_total_amount: toDollar(totalAmount).toString(),
+      biotech_company_name: biotechInvoice.biotech.name,
+      due_at: moment(biotechInvoice.due_at).format('ll'),
+      button_url: `${app_env.APP_URL}/app/invoices/${biotechInvoice.id}`
+    },
+    receiverEmail: r.user.email,
+  }));
+  const notificationData = receivers.map((r) => {
+    return createNewBiotechInvoiceNotificationJob({
+      recipient_id: r.id,
+      invoice_id: biotechInvoice.id,
+      invoice_number: biotechInvoice.invoice_number,
+      project_title: biotechInvoice.biotech_invoice_items[0].milestone?.quote.project_connection.project_request.title as string,
+    })
+  })
+  bulkNewBiotechInvoiceNoticeEmail(emailData);
+  createNotificationQueueJob({ data: notificationData });
+
+  return biotechInvoice;
 }
 
 const biotechInvoiceService = {
