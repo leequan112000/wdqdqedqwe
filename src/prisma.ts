464101852d68e1258a162fd5bd72fe6d588b7b76
Haosiong Ng <hao@cro-matic.com>
2024-02-28 09:12:58 +0800

    chore: replace NODE_ENV with APP_ENV

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,7 +1,58 @@
-import { PrismaClient as PrismaClientMainDb } from '@prisma/client';
-import { PrismaClient as PrismaClientCRODb } from '../prisma-cro/generated/client';
+import { PrismaClient as PrismaClientMainDb } from "@prisma/client";
+import { PrismaClient as PrismaClientCRODb } from "../prisma-cro/generated/client";
 
-const prisma = new PrismaClientMainDb({ log: ['info'] });
-const prismaCRODb = new PrismaClientCRODb({ log: ['info'] });
+const prisma = new PrismaClientMainDb({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log("Query: " + e.query);
+  console.log("Params: " + e.params);
+  console.log("Duration: " + e.duration + "ms");
+});
+
+const prismaCRODb = new PrismaClientCRODb({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+prismaCRODb.$on("query", (e) => {
+  console.log("Query: " + e.query);
+  console.log("Params: " + e.params);
+  console.log("Duration: " + e.duration + "ms");
+});
 
 export { prisma, prismaCRODb };
