27f993800057caafda042712b549fac5d0ee79d8
Shi Jin Ting <shijinting96@gmail.com>
2024-04-18 17:00:29 +0800

    Revert making biotech_id nullable in customers table

--- prisma-main/migrations/20240418065955_make_customers_biotech_id_column_nullable/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "customers" DROP CONSTRAINT "customers_biotech_id_fkey";
-
--- AlterTable
-ALTER TABLE "customers" ALTER COLUMN "biotech_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "customers" ADD CONSTRAINT "customers_biotech_id_fkey" FOREIGN KEY ("biotech_id") REFERENCES "biotechs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
