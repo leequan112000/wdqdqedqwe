894e4bb1affdec8bfae7b0d666503459cc52997e
Shi Jin Ting <shijinting96@gmail.com>
2024-05-13 16:45:22 +0800

    Updated vendorServicesQuery order by services name

--- src/controller/sitemap/vendor-services.ts
+++ src/controller/sitemap/vendor-services.ts
@@ -3,14 +3,13 @@ import { prismaCRODb } from '../../prisma';
 
 export const vendorServicesQuery = async (req: Request, res: Response): Promise<void> => {
   const apiKey = req.query.api_key
-  console.log(apiKey);
   if (!apiKey || apiKey !== process.env.CROMATIC_API_KEY) {
     res.status(400).send('API Error: Unauthorized');
     return;
   }
 
   try {
-    const subspecialties = await prismaCRODb.subspecialty.findMany();
+    const subspecialties = await prismaCRODb.subspecialty.findMany({ orderBy: { name: 'asc' } });
     res.status(200).json(subspecialties.map(s => s.name));
   } catch (error) {
     if (error instanceof Error) {
