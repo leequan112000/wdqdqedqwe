18a0124d3006180a5d88c687e13559939edb8695
leequan <leequan@cromatic.bio>
2024-07-22 11:36:10 +0800

    mutation in projectConnection resolver => collaborator.service and projectConnection.service

--- src/services/collaborator/collaborator.service.ts
+++ src/services/collaborator/collaborator.service.ts
@@ -12,7 +12,21 @@ import {
 import { PermissionDeniedError } from '../../graphql/errors/PermissionDeniedError';
 import invariant from '../../helper/invariant';
 import { InternalError } from '../../graphql/errors/InternalError';
-import { ServiceContext } from '../../types/context';
+import { Context, ServiceContext } from '../../types/context';
+import {
+  checkAllowAddProjectCollaborator,
+  checkAllowRemoveProjectCollaborator,
+} from '../../helper/accessControl';
+import { app_env } from '../../environment';
+import {
+  customerInvitationEmail,
+  projectCollaboratorInvitationEmail,
+  vendorMemberInvitationByUserEmail,
+} from '../../mailer';
+import createCollaboratedNotification from '../../notification/collaboratedNotification';
+import { PublicError } from '../../graphql/errors/PublicError';
+import { createResetPasswordToken } from '../../helper/auth';
+import { getUserFullName, createResetPasswordUrl } from '../../helper/email';
 
 type SetCustomerRoleAsAdminArgs = {
   biotech_id: string;
@@ -361,6 +375,345 @@ const deleteNewUser = async (args: DeleteNewUserArgs, ctx: ServiceContext) => {
   await deleteRolesForUser(args.user_id);
 };
 
+type AddProjectCollaboratorArgs = {
+  project_connection_id: string;
+  user_id: string;
+};
+
+const addProjectCollaborator = async (
+  args: AddProjectCollaboratorArgs,
+  context: Context,
+) => {
+  const { project_connection_id, user_id } = args;
+
+  await checkAllowAddProjectCollaborator(context);
+
+  const currentUser = await context.prisma.user.findFirst({
+    where: {
+      id: context.req.user_id,
+    },
+  });
+
+  invariant(currentUser, 'Current user not found.');
+
+  const user = await context.prisma.user.findFirst({
+    where: {
+      id: user_id,
+    },
+    include: {
+      customer: true,
+      vendor_member: true,
+      notifications: true,
+    },
+  });
+
+  invariant(user, 'User not found.');
+
+  if (user.customer || user.vendor_member) {
+    if (user?.customer) {
+      await context.prisma.customerConnection.upsert({
+        where: {
+          project_connection_id_customer_id: {
+            customer_id: user.customer.id,
+            project_connection_id,
+          },
+        },
+        create: {
+          customer_id: user.customer.id,
+          project_connection_id,
+        },
+        update: {
+          customer_id: user.customer.id,
+          project_connection_id,
+        },
+      });
+    } else if (user?.vendor_member) {
+      await context.prisma.vendorMemberConnection.upsert({
+        where: {
+          project_connection_id_vendor_member_id: {
+            vendor_member_id: user.vendor_member.id,
+            project_connection_id,
+          },
+        },
+        create: {
+          vendor_member_id: user.vendor_member.id,
+          project_connection_id,
+        },
+        update: {
+          vendor_member_id: user.vendor_member.id,
+          project_connection_id,
+        },
+      });
+    }
+
+    const projectConnection = await context.prisma.projectConnection.findFirst({
+      where: {
+        id: project_connection_id,
+      },
+      include: {
+        project_request: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+
+    if (projectConnection) {
+      projectCollaboratorInvitationEmail(
+        {
+          login_url: `${app_env.APP_URL}/app/project-connection/${project_connection_id}`,
+          inviter_full_name: `${currentUser.first_name} ${currentUser.last_name}`,
+          project_title: projectConnection.project_request.title,
+          receiver_full_name: `${user.first_name} ${user.last_name}`,
+        },
+        user.email,
+      );
+
+      try {
+        createCollaboratedNotification(
+          currentUser.id,
+          user.id,
+          projectConnection.id,
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      // no-op
+      // TODO: report to bug channel
+    }
+
+    return user;
+  }
+
+  throw new InternalError('User is not customer nor vendor member');
+};
+
+const removeProjectCollaborator = async (
+  args: AddProjectCollaboratorArgs,
+  context: Context,
+) => {
+  const { project_connection_id, user_id } = args;
+
+  await checkAllowRemoveProjectCollaborator(context);
+
+  const user = await context.prisma.user.findFirst({
+    where: {
+      id: user_id,
+    },
+    include: {
+      customer: true,
+      vendor_member: true,
+    },
+  });
+
+  invariant(user, 'User not found.');
+
+  if (user.customer) {
+    await context.prisma.customerConnection.delete({
+      where: {
+        project_connection_id_customer_id: {
+          customer_id: user.customer.id,
+          project_connection_id,
+        },
+      },
+    });
+    return user;
+  }
+
+  if (user.vendor_member) {
+    await context.prisma.vendorMemberConnection.delete({
+      where: {
+        project_connection_id_vendor_member_id: {
+          vendor_member_id: user.vendor_member.id,
+          project_connection_id,
+        },
+      },
+    });
+    return user;
+  }
+
+  throw new InternalError('User is not customer nor vendor member');
+};
+
+type InviteProjectCollaboratorViaEmailArgs = {
+  project_connection_id: string;
+  email: string;
+  name: string;
+  custom_message?: string | null;
+  role?: string | null;
+};
+export const inviteProjectCollaboratorViaEmail = async (
+  args: InviteProjectCollaboratorViaEmailArgs,
+  context: Context,
+) => {
+  const currentUserId = context.req.user_id;
+  const { project_connection_id, email, name, custom_message, role } = args;
+  const castedRole =
+    (role as CompanyCollaboratorRoleType) || CompanyCollaboratorRoleType.USER;
+
+  invariant(currentUserId, 'Current user id not found.');
+
+  // Check if current user has the permission to set the role.
+  if (castedRole) {
+    const roleEnum = Object.values(CompanyCollaboratorRoleType);
+    invariant(
+      roleEnum.includes(castedRole as CompanyCollaboratorRoleType),
+      'Invalid company collaborator role.',
+    );
+    await collaboratorService.checkPermissionToEditRole({
+      role: castedRole,
+      user_id: currentUserId,
+    });
+  }
+
+  await checkAllowAddProjectCollaborator(context);
+
+  const currentUser = await context.prisma.user.findFirst({
+    where: {
+      id: currentUserId,
+    },
+    include: {
+      customer: true,
+      vendor_member: true,
+    },
+  });
+
+  invariant(currentUser, 'Current user not found.');
+
+  const existingUser = await context.prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+    include: {
+      customer: true,
+      vendor_member: true,
+    },
+  });
+
+  if (existingUser) {
+    // If user exists and same company as the current user.
+    if (
+      (existingUser.customer &&
+        existingUser.customer?.biotech_id ===
+          currentUser.customer?.biotech_id) ||
+      (existingUser.vendor_member &&
+        existingUser.vendor_member.vendor_company_id ===
+          currentUser.vendor_member?.vendor_company_id)
+    ) {
+      throw new PublicError('User already exists.');
+    } else {
+      // If user exists but not the same company as the current user.
+      throw new PublicError(
+        'Please make sure the user is belong to your company.',
+      );
+    }
+  }
+
+  const resetTokenExpiration = new Date().getTime() + 7 * 24 * 60 * 60 * 1000;
+  const resetToken = createResetPasswordToken();
+
+  const isBiotech = !!currentUser.customer;
+  const isVendor = !!currentUser.vendor_member;
+
+  // If user doesn't exists
+  // 1. Create new user
+  // 2. Create customer/vendor member connection
+  // 3. Send invitation email
+  return await context.prisma.$transaction(async (trx) => {
+    const splitName = args.name.split(' ');
+    const firstName = splitName[0];
+    const lastName =
+      splitName.length === 1 ? '' : splitName[splitName.length - 1];
+
+    const newUser = await trx.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        reset_password_token: resetToken,
+        reset_password_expiration: new Date(resetTokenExpiration),
+        customer: isBiotech
+          ? {
+              create: {
+                biotech_id: currentUser.customer!.biotech_id,
+                customer_connections: {
+                  create: {
+                    project_connection_id,
+                  },
+                },
+              },
+            }
+          : undefined,
+        vendor_member: isVendor
+          ? {
+              create: {
+                vendor_company_id: currentUser.vendor_member!.vendor_company_id,
+                vendor_member_connections: {
+                  create: {
+                    project_connection_id,
+                  },
+                },
+              },
+            }
+          : undefined,
+      },
+      include: {
+        customer: true,
+        vendor_member: true,
+      },
+    });
+    const emailMessage = custom_message || '';
+
+    const newUserFullName = getUserFullName(newUser);
+    const resetPasswordUrl = createResetPasswordUrl(resetToken);
+    const currentUserFullName = getUserFullName(currentUser);
+
+    // Send email
+    if (isBiotech) {
+      customerInvitationEmail(
+        {
+          inviter_full_name: currentUserFullName,
+          inviter_message: emailMessage,
+          login_url: resetPasswordUrl,
+          receiver_full_name: newUserFullName,
+        },
+        newUser.email,
+      );
+    }
+    if (isVendor) {
+      vendorMemberInvitationByUserEmail(
+        {
+          inviter_full_name: currentUserFullName,
+          inviter_message: emailMessage,
+          login_url: resetPasswordUrl,
+          receiver_full_name: newUserFullName,
+        },
+        newUser.email,
+      );
+    }
+
+    // Update company role and casbin role
+    await collaboratorService.updateUserRole(
+      {
+        role: castedRole,
+        user_id: newUser.id,
+      },
+      { prisma: trx },
+    );
+
+    // Create notification
+    createCollaboratedNotification(
+      currentUser.id,
+      newUser.id,
+      project_connection_id,
+    );
+
+    return newUser;
+  });
+};
+
 const collaboratorService = {
   setCustomerAsAdmin,
   setCustomerAsUser,
@@ -371,6 +724,9 @@ const collaboratorService = {
   checkPermissionToEditRole,
   updateUserRole,
   deleteNewUser,
+  addProjectCollaborator,
+  removeProjectCollaborator,
+  inviteProjectCollaboratorViaEmail,
 };
 
 export default collaboratorService;
--- src/services/projectConnection/projectConnection.service.ts
+++ src/services/projectConnection/projectConnection.service.ts
@@ -1,4 +1,18 @@
-import { ServiceContext } from '../../types/context';
+import { app_env } from '../../environment';
+import { PermissionDeniedError } from '../../graphql/errors/PermissionDeniedError';
+import { PublicError } from '../../graphql/errors/PublicError';
+import { hasPermission } from '../../helper/casbin';
+import {
+  CasbinAct,
+  CasbinObj,
+  ProjectConnectionVendorStatus,
+  ProjectRequestStatus,
+} from '../../helper/constant';
+import invariant from '../../helper/invariant';
+import { sendVendorAcceptProjectNoticeEmail } from '../../mailer/projectRequest';
+import createAcceptRequestNotification from '../../notification/acceptRequestNotification';
+import { ServiceContext, Context } from '../../types/context';
+import chatService from '../chat/chat.service';
 
 type CheckIfUserInProjectConnectionArgs = {
   user_id: string;
@@ -7,11 +21,11 @@ type CheckIfUserInProjectConnectionArgs = {
 
 export const checkIfUserInProjectConnection = async (
   args: CheckIfUserInProjectConnectionArgs,
-  ctx: ServiceContext,
+  context: ServiceContext,
 ) => {
   const { project_connection_id, user_id } = args;
 
-  const cc = await ctx.prisma.customerConnection.findFirst({
+  const cc = await context.prisma.customerConnection.findFirst({
     where: {
       customer: {
         user_id,
@@ -20,7 +34,7 @@ export const checkIfUserInProjectConnection = async (
     },
   });
 
-  const vmc = await ctx.prisma.vendorMemberConnection.findFirst({
+  const vmc = await context.prisma.vendorMemberConnection.findFirst({
     where: {
       vendor_member: {
         user_id,
@@ -30,4 +44,202 @@ export const checkIfUserInProjectConnection = async (
   });
 
   return !!cc || !!vmc;
+};
+
+type AcceptProjectConnectionArgs = {
+  id: string;
+};
+
+export const acceptProjectConnection = async (
+  args: AcceptProjectConnectionArgs,
+  context: Context,
+) => {
+  const currentDate = new Date();
+  const currentUserId = context.req.user_id;
+  invariant(currentUserId, 'Current user id not found.');
+  const allowAcceptProjectConnection = await hasPermission(
+    currentUserId,
+    CasbinObj.PROJECT_CONNECTION,
+    CasbinAct.WRITE,
+  );
+  invariant(allowAcceptProjectConnection, new PermissionDeniedError());
+
+  const projectConnection = await context.prisma.projectConnection.findFirst({
+    where: {
+      id: args.id,
+    },
+    include: {
+      customer_connections: true,
+      vendor_member_connections: true,
+      project_request: true,
+    },
+  });
+  invariant(projectConnection, 'Project connection not found.');
+
+  if (
+    projectConnection.expired_at &&
+    currentDate >= projectConnection.expired_at
+  ) {
+    throw new PublicError('You can no longer accept this request');
+  }
+
+  const updatedProjectConnection = await context.prisma.$transaction(
+    async (trx) => {
+      if (
+        projectConnection.project_request.status ===
+        ProjectRequestStatus.WITHDRAWN
+      ) {
+        throw new PublicError('Project request has been withdrawn');
+      }
+
+      if (
+        projectConnection.project_request.status ===
+        ProjectRequestStatus.PROCESSING
+      ) {
+        await trx.projectRequest.update({
+          where: {
+            id: projectConnection.project_request_id,
+          },
+          data: {
+            status: ProjectRequestStatus.MATCHED,
+          },
+        });
+      }
+
+      let chat = await trx.chat.findFirst({
+        where: {
+          project_connection_id: projectConnection.id,
+        },
+      });
+
+      if (!chat) {
+        chat = await trx.chat.create({
+          data: {
+            biotech_id: projectConnection.project_request.biotech_id,
+            vendor_company_id: projectConnection.vendor_company_id,
+            project_connection_id: projectConnection.id,
+          },
+        });
+      }
+
+      await chatService.createSystemMessage(
+        {
+          chat_id: chat.id,
+          content:
+            "You've found a match! Start a conversation with your partner to kick off your project.",
+        },
+        { prisma: trx },
+      );
+
+      return await trx.projectConnection.update({
+        where: {
+          id: args.id,
+        },
+        data: {
+          vendor_status: ProjectConnectionVendorStatus.ACCEPTED,
+        },
+      });
+    },
+  );
+
+  const vendor = await context.prisma.vendorMember.findFirst({
+    where: {
+      user_id: currentUserId,
+    },
+    include: {
+      vendor_company: true,
+    },
+  });
+
+  if (vendor) {
+    // notify biotech members
+    const receivers = await context.prisma.user.findMany({
+      where: {
+        customer: {
+          id: {
+            in: projectConnection.customer_connections.map(
+              (cc) => cc.customer_id,
+            ),
+          },
+        },
+        OR: [
+          { deactivated_at: null },
+          {
+            deactivated_at: {
+              gt: new Date(),
+            },
+          },
+        ],
+      },
+    });
+
+    // send email and notification to biotech members
+    await Promise.all(
+      receivers.map(async (receiver) => {
+        await sendVendorAcceptProjectNoticeEmail(
+          {
+            login_url: `${app_env.APP_URL}/app/project-connection/${projectConnection.id}`,
+            receiver_full_name: `${receiver.first_name} ${receiver.last_name}`,
+            project_title: projectConnection.project_request.title,
+            vendor_company_name: vendor.vendor_company?.name as string,
+          },
+          receiver.email,
+        );
+
+        await createAcceptRequestNotification(
+          currentUserId,
+          receiver.id,
+          projectConnection.id,
+        );
+      }),
+    );
+  }
+
+  return updatedProjectConnection;
+};
+
+export const declineProjectConnection = async (
+  args: AcceptProjectConnectionArgs,
+  context: Context,
+) => {
+  const currentUserId = context.req.user_id;
+  invariant(currentUserId, 'Current user id not found.');
+  const allowDeclineProjectConnection = await hasPermission(
+    currentUserId,
+    CasbinObj.PROJECT_CONNECTION,
+    CasbinAct.WRITE,
+  );
+  invariant(allowDeclineProjectConnection, new PermissionDeniedError());
+
+  const currentDate = new Date();
+  const projectConnection = await context.prisma.projectConnection.findFirst({
+    where: {
+      id: args.id,
+    },
+  });
+  invariant(projectConnection, 'Project connection not found.');
+  // Check for expiry if project connection has never responsed.
+  if (
+    projectConnection.vendor_status === ProjectConnectionVendorStatus.PENDING &&
+    projectConnection.expired_at &&
+    currentDate >= projectConnection.expired_at
+  ) {
+    throw new PublicError('You can no longer decline this request');
+  }
+  const updatedProjectConnection =
+    await context.prisma.projectConnection.update({
+      where: {
+        id: args.id,
+      },
+      data: {
+        vendor_status: ProjectConnectionVendorStatus.DECLINED,
+      },
+    });
+  return updatedProjectConnection;
+};
+
+export const projectConnectionService = {
+  checkIfUserInProjectConnection,
+  acceptProjectConnection,
+  declineProjectConnection,
 };
