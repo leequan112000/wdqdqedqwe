6adeae1918ad084dc7d7a4fa29a9f2f371ace93c
Haosiong Ng <hao@cro-matic.com>
2024-08-05 11:33:19 +0800

    Create paid vendor and send sign up link mutation

---
+++ src/graphql-admin/resolvers/paidVendor.resolvers.ts
@@ -0,0 +1,67 @@
+import { createResetPasswordToken } from '../../helper/auth';
+import { createResetPasswordUrl } from '../../helper/email';
+import invariant from '../../helper/invariant';
+import { Context } from '../../types/context';
+import { Resolvers } from '../generated';
+
+const resolvers: Resolvers<Context> = {
+  Mutation: {
+    createPaidVendor: async (_, args, context) => {
+      const { email: emailArgs, company_name } = args;
+      const email = emailArgs.toLowerCase();
+
+      const paidVendor = await context.prisma.paidVendors.create({
+        data: {
+          email,
+          company_name: company_name || undefined,
+          user: {
+            create: {
+              email,
+            },
+          },
+        },
+      });
+
+      return paidVendor;
+    },
+    sendPaidVendorSignUpLink: async (_, args, context) => {
+      const { id } = args;
+
+      const paidVendor = await context.prisma.paidVendors.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      invariant(paidVendor, 'Vendor not found.');
+
+      const resetTokenExpiration =
+        new Date().getTime() + 7 * 24 * 60 * 60 * 1000;
+      const resetToken = createResetPasswordToken();
+
+      await context.prisma.paidVendors.update({
+        data: {
+          user: {
+            update: {
+              reset_password_token: resetToken,
+              reset_password_expiration: new Date(resetTokenExpiration),
+              reset_password_sent_at: new Date(),
+            },
+          },
+        },
+        where: {
+          id,
+        },
+      });
+
+      const resetPasswordUrl = createResetPasswordUrl(resetToken);
+
+      // TODO: Send email
+      console.log(resetPasswordUrl);
+
+      return true;
+    },
+  },
+};
+
+export default resolvers;
