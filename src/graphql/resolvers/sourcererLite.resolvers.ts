79a4ec7e785953396246cfdd2c2963edc28ab961
Shi Jin Ting <shijinting96@gmail.com>
2024-08-21 14:54:12 +0800

    Updated logic to auto use corrected spell checked keyword to search right away

--- src/graphql/resolvers/sourcererLite.resolvers.ts
+++ src/graphql/resolvers/sourcererLite.resolvers.ts
@@ -5,7 +5,14 @@ import sourcererLiteService from '../../services/sourcererLite/sourcererLite.ser
 const resolvers: Resolvers<Context> = {
   Query: {
     sourcererLiteSearch: async (_, args, context) => {
-      const { keyword, fingerprint, ip_address, first, after } = args;
+      const {
+        keyword,
+        fingerprint,
+        ip_address,
+        first,
+        after,
+        disable_spellcheck,
+      } = args;
 
       await sourcererLiteService.checkRateLimit(
         { keyword, fingerprint, ip_address },
@@ -22,20 +29,20 @@ const resolvers: Resolvers<Context> = {
         });
 
       if (!isSubspecialtyExist) {
-        const subspecialties =
-          (await sourcererLiteService.searchSubspecialtiesSemantically({
-            search_term: keyword,
-          })) as { id: string; name: string }[];
-
-        const subspecialty_ids = subspecialties.map((s) => s.id);
-        const subspecialty_names = subspecialties.map((s) => s.name);
-
         let did_you_mean_suggestion = '';
-        if (subspecialty_ids.length > 0) {
+        if (!disable_spellcheck) {
           did_you_mean_suggestion = await sourcererLiteService.checkSpelling({
             keyword,
           });
         }
+
+        const subspecialties =
+          (await sourcererLiteService.searchSubspecialtiesSemantically({
+            search_term: did_you_mean_suggestion || keyword,
+          })) as { id: string; name: string }[];
+
+        const subspecialty_ids = subspecialties.map((s) => s.id);
+        const subspecialty_names = subspecialties.map((s) => s.name);
 
         const result = await sourcererLiteService.matchVendorByServices(
           { subspecialty_ids, subspecialty_names, first, after, is_paid_user },
