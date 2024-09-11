406512ed70cd3995b73d7e712b585ea11f576eed
leequan <leequan@cromatic.bio>
2024-09-11 20:21:27 +0800

    create UserPseudonym table

---
+++ prisma-main/migrations/20240910154632_create_user_pseudonym_table/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "user_pseudonyms" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "user_id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "phone_number" TEXT,
+    "country_code" TEXT,
+
+    CONSTRAINT "user_pseudonyms_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_pseudonyms_user_id_key" ON "user_pseudonyms"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_pseudonyms_email_key" ON "user_pseudonyms"("email");
+
+-- AddForeignKey
+ALTER TABLE "user_pseudonyms" ADD CONSTRAINT "user_pseudonyms_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
